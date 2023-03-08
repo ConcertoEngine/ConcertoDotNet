01081e8bb11edb29a8a43715f134080cf0f5508f
Arthur Vasseur <arthur.vasseur@epitech.eu>
2023-03-08 09:25:47 +0900

    feat: adding unit tests

---
+++ Tests/HostFXR.cpp
@@ -0,0 +1,68 @@
+//
+// Created by arthur on 07/03/2023.
+//
+
+#include <filesystem>
+#include <gtest/gtest.h>
+#include "HostFXR.hpp"
+//TODO: disable multithreaded tests
+using namespace Concerto::DotNet;
+
+TEST(HostFXR, LoadHostFxr)
+{
+	try
+	{
+		std::string currentPath = std::filesystem::current_path().string();
+		HostFXR hostFxr(currentPath, "DotNetLib.runtimeconfig.json");
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		FAIL();
+	}
+}
+
+TEST(HostFXR, LoadAssembly)
+{
+	try
+	{
+		std::string currentPath = std::filesystem::current_path().string();
+		HostFXR hostFxr(currentPath, "DotNetLib.runtimeconfig.json");
+		Assembly assembly = hostFxr.LoadDotNetAssembly("DotNetLib.dll", "DotNetLib");
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		FAIL();
+	}
+}
+
+TEST(HostFXR, InvokeAssembly)
+{
+	try
+	{
+		std::string currentPath = std::filesystem::current_path().string();
+		HostFXR hostFxr(currentPath, "DotNetLib.runtimeconfig.json");
+		Assembly assembly = hostFxr.LoadDotNetAssembly("DotNetLib.dll", "DotNetLib");
+		struct lib_args
+		{
+			const char_t* message;
+			int number;
+		};
+		lib_args args{
+			.message = L"This string is passed from C++ to C#",
+			.number = 42
+		};
+
+		int x = assembly.Invoke<int>("Hello",(void*)&args, (int)sizeof(args));
+		EXPECT_EQ(x, 0);
+
+		x = assembly.Invoke<int>("Hello", (void*)&args, (int)sizeof(int)); // wrong size
+		EXPECT_EQ(x, 1);
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		FAIL();
+	}
+}
---
+++ Tests/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by arthur on 07/03/2023.
+//
+
+
+#include <gtest/gtest.h>
+
+int main()
+{
+	::testing::InitGoogleTest();
+	return RUN_ALL_TESTS();
+}
